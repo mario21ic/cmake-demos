16828b190ca6f1966b2d48abd0a73618abd9e395
mario21ic <mario21ic@gmail.com>
2018-11-12 10:51:28 -0500

    feat(step 4) adding system introspection

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,5 +1,8 @@
 #include "MathFunctions.h"
+//#include "TutorialConfig.h"
 #include <stdio.h>
+
+#include <math.h>
 
 // a hack square root calculation using simple operations
 double mysqrt(double x)
@@ -9,6 +12,12 @@ double mysqrt(double x)
   }
 
   double result;
+
+// if we have both log and exp then use them
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  result = exp(log(x) * 0.5);
+  fprintf(stdout, "Computing sqrt of %g to be %g using log\n", x, result);
+#else
   double delta;
   result = x;
 
@@ -22,6 +31,7 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     fprintf(stdout, "Computing sqrt of %g to be %g\n", x, result);
   }
+#endif
   return result;
 }
 
